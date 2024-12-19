662a2855f894d431e64c43d77e09bd7a3dd0d797
Mateusvspm <sousamateusv@gmail.com>
2024-12-19 10:25:37 -0300

    concluido

--- PlayMusic.java/src/App.java
+++ PlayMusic.java/src/App.java
@@ -1,15 +1,14 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
 
-
 import dominio.musica;
 import dominio.album;
 import dominio.artista;
-
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -18,19 +17,20 @@ public class App {
         musica1.setNome("Summer Eletrohits - Don't know why");
         musica1.setGenero("Eletronica");
         musica1.setDuração(120);
-        musica1.setArquivoAudio("./assets/y2meta.net_320kbps-moony-i-dont-know-why-summer-eletrohits-5.mp3");
+        musica1.setArquivoAudio("./assets/y2meta.net_320kbps-moony-i-dont-know-why-summer-eletrohits-5.wav");
 
         musica musica2 = new musica();
         musica2.setNome("Summer Eletrohits - Stereo Love");
         musica2.setGenero("Eletronica");
         musica2.setDuração(120);
-        musica2.setArquivoAudio("./assets/y2meta.net_320kbps-edward-maya-and-vika-jigulina-stereo-love-official-music-video.mp3");
+        musica2.setArquivoAudio(
+                "./assets/y2meta.net_320kbps-edward-maya-and-vika-jigulina-stereo-love-official-music-video.wav");
 
         musica musica3 = new musica();
         musica3.setNome("Summer Eletrohits - If You");
         musica3.setGenero("Eletronica");
         musica3.setDuração(120);
-        musica3.setArquivoAudio("./assets/y2meta.net_320kbps-magic-box-if-you-official-video.mp3");
+        musica3.setArquivoAudio("./assets/y2meta.net_320kbps-magic-box-if-you-official-video.wav");
 
         album album1 = new album();
         album1.setNome("Primeiro album");
@@ -38,7 +38,7 @@ public class App {
         album1.addMusica(musica1);
         album1.addMusica(musica2);
         album1.addMusica(musica3);
-        
+
         artista SummerEletroHits = new artista();
         SummerEletroHits.setNome("Summer Eletrohits");
         SummerEletroHits.addAlbum(album1);
@@ -46,10 +46,21 @@ public class App {
         System.out.println("Abrindo PlayMusic");
 
         AudioPlayer player = new AudioPlayer();
-        player.loadAudio(SummerEletroHits.getAlbuns().get(0).getMusicas().get(0).getArquivoAudio()); // Carregue o áudio ao iniciar
+        player.loadAudio(SummerEletroHits.getAlbuns().get(0).getMusicas().get(0).getArquivoAudio()); // Carregue o áudio
+                                                                                                     // ao iniciar
 
-        // Cria o botão Play/Stop e configura sua ação
+        // Cria os botões Play/Stop e Next e configura suas ações
         JButton playStopButton = new JButton("Play");
+        JButton nextButton = new JButton("Next");
+
+        // Lista de músicas e índice atual
+        List<musica> musicas = SummerEletroHits.getAlbuns().get(0).getMusicas();
+        int[] musicaAtual = { 0 };
+
+        // Inicializa o player com a primeira música
+        player.loadAudio(musicas.get(musicaAtual[0]).getArquivoAudio());
+
+        // Configura ação do botão Play/Stop
         playStopButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -62,17 +73,34 @@ public class App {
                 }
             }
         });
-        
-        ImageIcon icon = new ImageIcon("./assets/music.png");
-        // Exibe um JOptionPane com o botão Play/Stop
+
+        // Configura ação do botão Next
+        nextButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                player.stopAudio(); // Para a música atual
+                musicaAtual[0] = (musicaAtual[0] + 1) % musicas.size(); // Avança para a próxima música
+                player.loadAudio(musicas.get(musicaAtual[0]).getArquivoAudio()); // Carrega a nova música
+                JOptionPane.showMessageDialog(
+                        null,
+                        "Tocando: " + musicas.get(musicaAtual[0]).getNome(),
+                        "PlayMusic",
+                        JOptionPane.INFORMATION_MESSAGE);
+                playStopButton.setText("Play");
+            }
+        });
+
+        // Exibe um JOptionPane com os botões Play/Stop/Next
+        ImageIcon icon = new ImageIcon("./src/assets/music.png");
         JOptionPane.showOptionDialog(
                 null,
-                SummerEletroHits.getAlbuns().get(0).getMusicas().get(0).getNome(),
+                "Escolha uma ação:",
                 "PlayMusic",
                 JOptionPane.DEFAULT_OPTION,
                 JOptionPane.PLAIN_MESSAGE,
                 icon,
-                new Object[] { playStopButton }, playStopButton);
+                new Object[] { playStopButton, nextButton },
+                playStopButton);
 
         // Fecha o clip de áudio ao encerrar o programa
         if (player.audioClip != null) {
@@ -80,4 +108,5 @@ public class App {
         }
 
     }
+
 }
--- PlayMusic.java/src/AudioPlayer.java
+++ PlayMusic.java/src/AudioPlayer.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
@@ -10,100 +9,58 @@ import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
-import dominio.musica;
-
-
-
 
 public class AudioPlayer {
-    private List<musica> listaDeReproducao;
-    public Clip audioClip;
-    public boolean isPlaying = false;
-    private int indice;
-  
-    public void loadAudio(String filePath) {
-      System.out.println("loadAudio: " + filePath);
-      try {
-        // Carrega o arquivo de áudio
-        File audioFile = new File(filePath);
-        AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
-  
-        // Configura o Clip
-        AudioFormat format = audioStream.getFormat();
-        DataLine.Info info = new DataLine.Info(Clip.class, format);
-        audioClip = (Clip) AudioSystem.getLine(info);
-  
-        audioClip.open(audioStream);
-      } catch (UnsupportedAudioFileException e) {
-        System.out.println("O formato do arquivo de áudio não é suportado.");
-        e.printStackTrace();
-      } catch (LineUnavailableException e) {
-        System.out.println("A linha de áudio não está disponível.");
-        e.printStackTrace();
-      } catch (IOException e) {
-        System.out.println("Erro ao ler o arquivo de áudio.");
-        e.printStackTrace();
-      }
-    }
-  
-    public void playAudio() {
-      System.out.println("playAudio");
-      if (audioClip != null && !isPlaying) {
-        audioClip.setFramePosition(0); // Reinicia o áudio do começo
-        System.out.println("playAudio start");
-        audioClip.start();
-        isPlaying = true;
-      }
-    }
-  
-    public void stopAudio() {
-      System.out.println("stopAudio");
-      if (audioClip != null && isPlaying) {
-        System.out.println("stopAudio stop");
-        audioClip.stop();
-        isPlaying = false;
-      }
-    }
-  
-    public void previousAudio (){
-  
-      System.out.println("previousAudio");
-      stopAudio();
-  
-      if(this.indice == 0){
-      indice = listaDeReproducao.size() - 1;
-      loadAudio(listaDeReproducao.get(indice).getArquivoAudio());
-      playAudio();
+  public Clip audioClip;
+  public boolean isPlaying = false;
 
-    }else if(this.indice <= listaDeReproducao.size() - 1){
-      indice --;
-      loadAudio(listaDeReproducao.get(indice).getArquivoAudio());
-      playAudio();
+  public void loadAudio(String filePath) {
+    System.out.println("loadAudio: " + filePath);
+    try {
+      // Carrega o arquivo de áudio
+      File audioFile = new File(filePath);
+      AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
 
+      // Configura o Clip
+      AudioFormat format = audioStream.getFormat();
+      DataLine.Info info = new DataLine.Info(Clip.class, format);
+      audioClip = (Clip) AudioSystem.getLine(info);
+
+      audioClip.open(audioStream);
+    } catch (UnsupportedAudioFileException e) {
+      System.out.println("O formato do arquivo de áudio não é suportado.");
+      e.printStackTrace();
+    } catch (LineUnavailableException e) {
+      System.out.println("A linha de áudio não está disponível.");
+      e.printStackTrace();
+    } catch (IOException e) {
+      System.out.println("Erro ao ler o arquivo de áudio.");
+      e.printStackTrace();
     }
   }
 
-  public void nextAudio (){
-
-    System.out.println("nextAudio");
-    stopAudio();
-
-    if(this.indice < listaDeReproducao.size() - 1){
-      indice ++;
-      loadAudio(listaDeReproducao.get(indice).getArquivoAudio());
-      playAudio();
-
-    }else if(this.indice == listaDeReproducao.size() - 1){
-      indice = 0;
-      loadAudio(listaDeReproducao.get(indice).getArquivoAudio());
-      playAudio();
-
-    }else{
-      System.out.println("Sua lista de reprodução está vazia!\nAdicione músicas para tocar...");
-
+  public void playAudio() {
+    System.out.println("playAudio");
+    if (audioClip != null && !isPlaying) {
+      audioClip.setFramePosition(0); // Reinicia o áudio do começo
+      System.out.println("playAudio start");
+      audioClip.start();
+      isPlaying = true;
     }
   }
 
-}  
+  public void stopAudio() {
+    System.out.println("stopAudio");
+    if (audioClip != null && isPlaying) {
+      System.out.println("stopAudio stop");
+      audioClip.stop();
+      isPlaying = false;
+    }
+  }
 
+  public void nextAudio() {
 
+    throw new UnsupportedOperationException("Unimplemented method 'nextAudio'");
+  }
+
+}
